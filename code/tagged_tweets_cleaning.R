6be13545c2ef4bdbc661726dfc385994fb4268b9
edriannliu <edrian@connect.hku.hk>
2023-11-30 14:51:34 +0800

    clean manually tagged tweets

--- code/tagged_tweets_cleaning.R
+++ code/tagged_tweets_cleaning.R
@@ -24,10 +24,29 @@ d_israel_0708$sentiment <- as.numeric(d_israel_0708$sentiment)
 
 d_tagged <- bind_rows(d_hamas_0721, d_israel_0708)
 
-write.csv(d_tagged, file = "output/manually_tagged_sentiment.csv",
+# clean emojis, etc.
+clean = function(text) {
+  text = iconv(text, "latin1", "ASCII", sub="") #change encoding
+  text = gsub("(@)\\w+", "", text) #remove numbers, alphabets after "@" (username)
+  text = gsub("(http|https)://.*", "", text) # remove links
+  text = gsub("[ \t]{2,}", "", text) #remove two blank spaces and tab
+  text = gsub("\\n"," ",text) # remove newline
+  text = gsub("\\s+"," ",text) # remove blank spaces
+  text = gsub("^\\s+|\\s+$","",text) # remove blank spaces
+  text = gsub("&.*;","",text) # remove special symbols and html
+  text = gsub("[^a-zA-Z0-9?!. ']","",text) # remove emojis
+}
+
+d_tagged_clean <- d_tagged |>
+  mutate(clean(d_tagged$text)) |>
+  rename("cleaned_text" = "clean(d_tagged$text)") |>
+  select(cleaned_text, sentiment) |>
+  distinct(cleaned_text, .keep_all = TRUE)
+
+write.csv(d_tagged_clean, file = "output/manually_tagged_sentiment.csv",
           row.names = FALSE)
 
-d_tagged_count <- d_tagged |>
+d_tagged_count <- d_tagged_clean |>
   group_by(sentiment) |>
   count()
 print(d_tagged_count)
